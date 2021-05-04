44eac861bf98c4937f4870e60427ce2272922099
何翰宇 <913861123@qq.com>
2021-05-04 13:00:12 +0800

    顺序表思路练习

--- 顺序表/MyArrayList.java
+++ 顺序表/MyArrayList.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 
+/**
+ * 简单的顺序表思路
+ */
 public class MyArrayList {
 
     //实例成员变量：如果不初始化默认值就是对应的0值
@@ -10,8 +13,8 @@ public class MyArrayList {
         this.elem = new int[5];
     }
 
-    public  boolean judge (int lens) {//判断增容
-        if(lens == this.elem.length) {
+    public boolean judge(){//判断是否要增容
+        if(this.usedSize == this.elem.length){
             return true;
         }else{
             return false;
@@ -20,24 +23,23 @@ public class MyArrayList {
     // 在 pos 位置新增元素
     public void add(int pos, int data) {
         //判断是否要增容
-        if(this.judge(this.usedSize)){
-            this.elem = Arrays.copyOf(this.elem,this.elem.length*2);
+        if(judge()){
+            this.elem = Arrays.copyOf(this.elem,this.elem.length * 2);
         }
-        //判断pow是否合法
-        if(pos < 0 || pos > this.usedSize) {
-            System.out.println("输入的pos下标不合法");
+        //判读那位合法性
+        if(pos < 0 || pos > this.usedSize){
+            System.out.println("pos位置不合法");
             return;
         }
-        //插入元素
-        for(int i = this.usedSize-1;pos <= i;i--) {
-            this.elem[i+1] = this.elem[i];
+        //判断元素是否要插入
+        for (int i = this.usedSize; i > pos; i++) {
+            this.elem[i] = this.elem[i+1];
         }
         this.elem[pos] = data;
         this.usedSize++;
-        System.out.println("添加成功");
     }
     // 打印顺序表
-    public void display(){
+    public void display() {
         for (int i = 0; i < this.usedSize; i++) {
             System.out.print(this.elem[i]+" ");
         }
@@ -62,24 +64,40 @@ public class MyArrayList {
         return -1;
     }
     // 获取 pos 位置的元素
-    public int getPos(int pos) {
-        for (int i = 0; i < this.usedSize; i++) {
-            if(this.elem[i] == pos){
-                return this.elem[i];
-            }
+    public int getPos(int pos) {//这里仅仅是针对正数情况
+        if(pos<0 || pos > this.usedSize){
+            System.out.println("pos位置不合法");
+            return -1;
         }
-        return -1;
+        return this.elem[pos];
     }
     // 给 pos 位置的元素设为 value
-    public void setPos(int pos, int value) { }
+    public void setPos(int pos, int value) {
+        if(pos < 0 || pos > this.usedSize){
+            System.out.println("pos位置不合法");
+            return;
+        }
+        this.elem[pos] = value;
+    }
     //删除第一次出现的关键字key
-    public void remove(int toRemove) { }
+    public void remove(int key) {
+        int index = search(key);//找元素下标
+        if(index == -1){
+            System.out.println("key关键字不存在");
+            return;
+        }
+        for (int i = index; i < this.usedSize; i++) {
+            this.elem[i] = this.elem[i+1];
+        }
+        this.usedSize--;
+    }
     // 获取顺序表长度
     public int size() {
-        return this.elem.length;
+        return this.usedSize;
     }
     // 清空顺序表
-    public void clear() {}
+    public void clear() {//这里只是比较简单的思路而已，如果里面是对象则需要值位 null
+        this.usedSize = 0;
+    }
 
 }
-
--- 顺序表/TestDemo.java
+++ 顺序表/TestDemo.java
@@ -1,22 +1,11 @@
 import java.util.Arrays;
 
 
+
 public class TestDemo {
 
     public static void main(String[] args) {
         //顺序表的对象
         MyArrayList myArrayList = new MyArrayList();
-        myArrayList.add(0,1);
-        myArrayList.add(1,2);
-        myArrayList.add(2,3);
-        myArrayList.add(3,4);
-        myArrayList.add(4,5);
-        myArrayList.add(5,6);
-        myArrayList.add(2,88);
-        myArrayList.display();
-        System.out.println(myArrayList.contains(81));
-        System.out.println(myArrayList.search(88));
-        System.out.println(myArrayList.getPos(6));
-        System.out.println(Arrays.toString(myArrayList.elem));
     }
 }
