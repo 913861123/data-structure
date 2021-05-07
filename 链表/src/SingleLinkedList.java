ce445a3d5c83d9bacc3d91c09dfcf7908063e0f3
何翰宇 <913861123@qq.com>
2021-05-07 23:52:40 +0800

    链表思路练习

--- 链表/src/SingleLinkedList.java
+++ 链表/src/SingleLinkedList.java
@@ -1,151 +1,137 @@
 
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * Java集合当中 LinkedList 底层是一个双向链表  这里只是用了一下名字
- * User: GaoBo
- * Date: 2021-04-27
- * Time: 19:20
- */
-
 class ListNode {
     public int val;//值
     public ListNode next;//存储下一个节点的地址  也就是说 这个是一个引用
 
-    public ListNode() {
+    public ListNode(){
 
     }
-
-    public ListNode(int val) {
+    public ListNode(int val){
         this.val = val;
     }
 }
 
-
 public class SingleLinkedList {
-
-    public ListNode head;//标识整个链表的头
-
-    //得到单链表的长度
-    public int size() {
-        int cont = 0;
-        ListNode tmp = this.head;
-        while(tmp != null){
-            cont++;
-            tmp = tmp.next;
-        }
-        return cont;
-    }
-
+    public ListNode head;//用来标识链表的头部
     //头插法
-    public void addFirst(int data) {
+    public void addFirst(int data){
         ListNode node = new ListNode(data);
         node.next = this.head;
         this.head = node;
     }
-
     //尾插法
-    public void addLast(int data) {
+    public void addLast(int data){
         ListNode node = new ListNode(data);
-        ListNode tmp = this.head;
-        if(tmp == null){//当链表为空时
-            this.head = node;
+
+        if(this.head == null){//当链表为空时
+                this.head = node;
         }else{
-            while(tmp.next != null){
-                tmp = tmp.next;
+            //找尾巴
+            ListNode cur = this.head;
+            while(cur.next != null){
+                cur = cur.next;
             }
-            tmp.next = node;
+            cur.next = node;
         }
-
     }
-
     //任意位置插入,第一个数据节点为0号下标
-    public void addIndex(int index, int data) {
-        //判断index位置合法性
-        if(index < 0 || index > this.size()){
-            System.out.println("index位置合法性");
+    public void addIndex(int index,int data){
+        //判断位置合法性
+        if(index < 0 || index >size()){
+            System.out.println("index位置不合法");
             return;
         }
-        //头插法
-        if(this.size() == 0){
+        //头插
+        if(index == 0){
             this.addFirst(data);
         }
-        //尾插法
-        if(this.size() == index){
+        //尾插
+        if(index == this.size()){
             this.addLast(data);
         }
+        //选择插入
         ListNode node = new ListNode(data);
-        ListNode tmp = findIndexSubOne(index);
-        node = tmp;
-        tmp = node.next;
+        ListNode cur = findIndexSubOne(index);
+        node.next = cur.next;
+        cur.next = node;
     }
 
     /**
-     * 让引用从头开始走 index-1步
+     * 从引用头开始，走 index-1 步
      * @param index
      * @return
      */
     public ListNode findIndexSubOne(int index){
-        ListNode tmp = this.head;
-        for (int i = 0; i < index-1; i++) {
-            tmp = tmp.next;
+        ListNode cur = this.head;
+        for(int i = 0; i < index-1; i++){
+                cur = cur.next;
         }
-        return tmp;
+        return cur;
     }
     //查找是否包含关键字key是否在单链表当中
-    public boolean contains(int key) {
-        ListNode tmp = this.head;
-        while(tmp != null){
-            if(tmp.val == key){
+    public boolean contains(int key){
+        ListNode cur = this.head;
+        while(cur != null){
+            if(cur.val == key){
                 return true;
             }
-            tmp = tmp.next;
+            cur = cur.next;
         }
         return false;
     }
-
     //删除第一次出现关键字为key的节点
-    public void remove(int key) {
-
-    }
-
-    //删除所有值为key的节点
-    public void removeAllKey(int key) {
-
-    }
-
-
-    public void clear() {
-
-    }
-
-    /**
-     * 穷举法，最low的方式 先建造一个链表出来，让你来感受一下
-     */
-    public void createList() {
-        ListNode listNode1 = new ListNode(12);//12
-        ListNode listNode2 = new ListNode(45);
-        ListNode listNode3 = new ListNode(6);
-        ListNode listNode4 = new ListNode(8);
-        ListNode listNode5 = new ListNode(5);
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
-        listNode3.next = listNode4;
-        listNode4.next = listNode5;
-        this.head = listNode1;//head引用  引用了 listNode1引用  引用的对象
-
-    }
-
-    /**
-     * 遍历链表：
-     */
-    public void display() {
+    public void remove(int key){
         ListNode cur = this.head;
-        //不能是this.head.next != null  这样写 最后一个节点是不能被打印的
-        while (cur != null) {
-            System.out.print(cur.val + " ");
+        while(cur != null){
+            if(cur.val == key){
+                if(cur == this.head){
+                    head = cur.next;
+                    cur = null;
+                }else if(cur.next == null){
+
+                }
+            }
+        }
+    }
+    //删除所有值为key的节点
+    public void removeAllKey(int key){
+        ListNode cur = this.head;
+        while(cur != null){
+            if(cur.val == key){
+                cur = cur.next;
+            }
+        }
+    }
+    //得到单链表的长度
+    public int size(){
+        ListNode cur = this.head;
+        int count = 0;
+        while(cur != null){
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+    //遍历链表
+    public void display(){
+        ListNode cur = this.head;
+        //不能是this.head.next != null  这样写最后一个节点是不能被打印的
+        while(cur != null){
+            System.out.print(cur.val+" ");
             cur = cur.next;
         }
         System.out.println();
     }
+    public void clear(){
+        //需要置空的节点
+        ListNode cur = this.head;
+        //一个一个置空
+        while(cur != null){
+            ListNode cunNext = cur.next;
+            cur.next = null;
+            cur = cunNext;
+        }
+        this.head = null;
+    }
 }
+
